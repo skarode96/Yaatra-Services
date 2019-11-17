45915bae716c1bf9ea931af73032ba4da3d11264
rhofix <robagwe@email.com>
2019-11-17 00:13:54 +0000

    settings.py changed APP to API

--- backend/service/settings.py
+++ backend/service/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'APP',
+    'API',
     'rest_framework',
     'rest_framework.authtoken'
 ]
