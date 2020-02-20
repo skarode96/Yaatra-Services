1eeacfac3e5130396adb459e1bb908f20a5d35c3
Roy, Srijan <roy.srijon2012@gmail.com>
2020-02-20 20:01:14 +0000

    changes for create journey

--- daily_commute/serializers.py
+++ daily_commute/serializers.py
@@ -7,11 +7,11 @@ class DailyCommuteSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = DailyCommute
-        fields = ['user','journey_title', 'source_long', 'source_lat', 'destination_lat', 'destination_long', 'start_time', 'pref_mode_travel', 'pref_gender', 'journey_frequency']
+        fields = ['user_id', 'journey_id', 'journey_title', 'source_long', 'source_lat', 'destination_lat', 'destination_long', 'start_time', 'pref_mode_travel', 'pref_gender', 'journey_frequency']
 
     def save(self):
         dailyCommuteDetails = DailyCommute(journey_title=self.validated_data['journey_title'],
-                           user=self.validated_data['user'],
+                           user=self.validated_data['user_id'],
                            source_long=self.validated_data['source_long'],
                            source_lat=self.validated_data['source_lat'],
                            destination_lat=self.validated_data['destination_lat'],
@@ -20,6 +20,7 @@ class DailyCommuteSerializer(serializers.ModelSerializer):
                            journey_frequency=self.validated_data['journey_frequency'],
                            pref_gender =self.validated_data['pref_gender'],
                            pref_mode_travel=self.validated_data['pref_mode_travel'],
+                           journey_id=self.validated_data['journey_id']
                            )
         dailyCommuteDetails.save()
         return dailyCommuteDetails
--- daily_commute/views.py
+++ daily_commute/views.py
@@ -1,3 +1,6 @@
+from decimal import Decimal
+from tkinter import Place
+
 from django.contrib.auth import authenticate
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.authtoken.models import Token
@@ -19,10 +22,11 @@ from .serializers import DailyCommuteSerializer
 import logging
 import socket
 from django.forms.models import model_to_dict
-
+import mpu
 
 host_name = socket.gethostname()
 machine_ip = socket.gethostbyname(host_name)
+
 
 @ratelimit.decorators.ratelimit(key='ip', rate='200/h')
 @csrf_exempt
@@ -60,7 +64,8 @@ def get_daily_commutes_for_user(request):
 @api_view(["GET"])
 def get_user_list_for_journey(request):
     logger = logging.getLogger()
-    logger.info("User-List-for-Journey: Request is being served by Instance: {} at IP: {}".format(host_name, machine_ip))
+    logger.info(
+        "User-List-for-Journey: Request is being served by Instance: {} at IP: {}".format(host_name, machine_ip))
     """
         This view should return the list of all daily commuters for the selected jouney_id
         input: journey_id (pk)
@@ -91,16 +96,19 @@ def get_user_list_for_journey(request):
     data = []
     return Response(data, status=HTTP_200_OK)
 
+
 @ratelimit.decorators.ratelimit(key='ip', rate='50/m')
 @csrf_exempt
 @api_view(["GET"])
 def daily_commuter_notification(request):
     logger = logging.getLogger()
-    logger.info("Create-Daily-Notification: Request is being served by Instance: {} at IP: {}".format(host_name, machine_ip))
+    logger.info(
+        "Create-Daily-Notification: Request is being served by Instance: {} at IP: {}".format(host_name, machine_ip))
     """
     Input: Notification   """
     data = []
     return Response(data, status=HTTP_200_OK)
+
 
 @ratelimit.decorators.ratelimit(key='ip', rate='50/m')
 @csrf_exempt
@@ -114,7 +122,8 @@ def create_daily_commute(request):
     if request.method == 'POST':
 
         if request.data.get('journey_title') is None or request.data.get('destination_lat') is None or \
-                request.data.get('start_time') is None or request.data.get('journey_frequency') is None or request.data.get('username') is None:
+                request.data.get('start_time') is None or request.data.get(
+            'journey_frequency') is None or request.data.get('username') is None:
             return Response({'message': 'Form Data is missing!',
                              'response': 'Error', },
                             status=HTTP_400_BAD_REQUEST)
@@ -128,17 +137,45 @@ def create_daily_commute(request):
         data = request.data.copy()
         user = User.objects.get(username=username)
         data['user'] = user.pk
-        serializer = DailyCommuteSerializer(data=data)
+        source_long, source_lat = request.data.get('source_long'), request.data.get('source_lat')
+        dest_long, dest_lat = request.data.get('destination_long'), request.data.get('destination_lat')
 
-        if serializer.is_valid():
-            dailyCommuteDetails = serializer.save()
-            dailyCommuteData = model_to_dict(dailyCommuteDetails,
-                                 fields=['journey_title', 'source_long', 'source_lat', 'destination_lat', 'destination_long', 'start_time', 'journey_frequency'])
-            dailyCommuteData['message'] = 'Journey creation Successful!'
-            dailyCommuteData['response'] = 'Success'
+        latlong_km = Decimal(0.0039)
+        source_long_min, source_long_max = Decimal(source_long) - latlong_km, Decimal(source_long) + latlong_km
+        source_lat_min, source_lat_max = Decimal(source_lat) - latlong_km, Decimal(source_lat) + latlong_km
+        dest_long_min, dest_long_max = Decimal(dest_long) - latlong_km, Decimal(dest_long) + latlong_km
+        dest_lat_min, dest_lat_max = Decimal(dest_lat) - latlong_km, Decimal(dest_lat) + latlong_km
+        nearby_travels = DailyCommute.objects.filter(source_lat__lte=source_lat_max, source_lat__gte=source_lat_min,
+                                                     source_long__lte=source_long_max, source_long__gte=source_long_min,
+                                                     destination_lat__lte=dest_lat_max,
+                                                     destination_lat__gte=dest_lat_min,
+                                                     destination_long__lte=dest_long_max,
+                                                     destination_long__gte=dest_long_min)
+
+        serializer = DailyCommuteSerializer(instance=nearby_travels, many=True)
+        nearby_travel_data = serializer.data[:]
+        if nearby_travels:
+            minimum_distance = 1000
+            for travel in nearby_travels:
+                distance = mpu.haversine_distance((travel.source_lat, travel.source_long), (source_lat, source_long))
+                if distance < minimum_distance:
+                    minimum_distance = distance
+                    nearest_journey_id = travel.id
         else:
-            dailyCommuteData = serializer.errors
+            max_journey_id = DailyCommute.objects.all().aggregate(max('journey_id'))
+            data['journey_id'] = max_journey_id
+            serializer = DailyCommuteSerializer(instance=data)
+            if serializer.is_valid():
+                dailyCommuteDetails = serializer.save()
+                dailyCommuteData = model_to_dict(dailyCommuteDetails,
+                                                 fields=['journey_id', 'journey_title', 'source_long', 'source_lat', 'destination_lat',
+                                                         'destination_long', 'start_time', 'journey_frequency'])
+                dailyCommuteData['message'] = 'Journey creation Successful!'
+                dailyCommuteData['response'] = 'Success'
+            else:
+                dailyCommuteData = serializer.errors
         return Response(dailyCommuteData, status=HTTP_201_CREATED)
+
 
 @ratelimit.decorators.ratelimit(key='ip', rate='50/m')
 @csrf_exempt
