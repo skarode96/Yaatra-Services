74264d157b62260d9a178b3999e6af0f902ad0c3
Rohan B <55564999+rohan-tcd@users.noreply.github.com>
2020-04-24 19:36:10 +0100

    refactoring test

--- deployment/DDoS_Test.py
+++ deployment/DDoS_Test.py
@@ -2,8 +2,6 @@ import requests
 import json
 import sys
 
-API_ENDPOINT = "http://192.168.0.221:4000/user/login/"
-
 API_ENDPOINT = sys.argv[1]
 USERNAME = sys.argv[2]
 PASSWORD = sys.argv[3]
@@ -14,8 +12,7 @@ data = {'username':USERNAME,
 count=0
 while True:
 	print("Request No. :", count)
-	count+=1
-# sending post request and saving response as response object 
+	count+=1 
 	reponse = requests.post(url = API_ENDPOINT, data = data)
 	if(reponse.status_code == 503):
 		print("Response :%s"%reponse.text)
